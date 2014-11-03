70ffcd82f0322552c10db7b3d1feee2e07f9c1f5
rayc5 <rayc5c5@gmail.com>
2014-11-03 11:23:40 +0900

    Initial Commit

---
+++ KVOPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KVOPractice
+//
+//  Created by 本間 光宣 on 2014/11/03.
+//  Copyright (c) 2014年 rayc5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KVOPractice/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KVOPractice
+//
+//  Created by 本間 光宣 on 2014/11/03.
+//  Copyright (c) 2014年 rayc5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
